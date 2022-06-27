db1b4f49cbbe9fc7de22b1eca04a4368dee78780
Bel Albuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-06-27 19:43:36 -0300

    implementa classe main

---
+++ src/bank/Bank.java
@@ -0,0 +1,30 @@
+package bank;
+
+public class Bank {
+    public static void main(String[] args) {
+        Executivo executivo = new Executivo();
+        Basico basico = new Basico();
+        Cobrador cobrador = new Cobrador();
+
+        System.out.println("Executivo");
+        executivo.consultaDeSaldo();
+        executivo.deposito();
+        executivo.pagamentosDeServico();
+        executivo.saqueEmDinheiro();
+        executivo.transferencia();
+
+        System.out.println("Basico");
+        basico.consultaDeSaldo();
+        basico.deposito();
+        basico.pagamentosDeServico();
+        basico.saqueEmDinheiro();
+        basico.transferencia();
+
+        System.out.println("Cobrador");
+        basico.consultaDeSaldo();
+        basico.deposito();
+        basico.pagamentosDeServico();
+        basico.saqueEmDinheiro();
+        basico.transferencia();
+    }
+}
