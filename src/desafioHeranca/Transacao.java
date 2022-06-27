4aa03b82c93afb734add0e3f8a066d9d4ce6514b
andherreraML <andrea.herrera@mercadolivre.com>
2022-06-27 19:16:30 -0300

    feat: interface transacao

---
+++ src/desafioHeranca/Transacao.java
@@ -0,0 +1,9 @@
+package desafioHeranca;
+
+public interface Transacao {
+    void fazerDeposito();
+    void fazerTransferencia();
+    void fazerSaqueEmDinheiro();
+    void fazerConsultaDeSaldo();
+    void fazerPagamentoDeServico();
+}
